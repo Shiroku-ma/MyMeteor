833070a8aa0197928ac7e581ddc07dd9f64bbac2
Shiroku-ma <y.sinpei0221@gmail.com>
2023-09-04 22:57:33 +0900

    first commit [Add] click to add a star

---
+++ make.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    strokeWeight(0);
+    fill(224,177,177)
+}
+
+var r = 0;
+
+function draw() {
+    
+}
+
+function mouseClicked(){
+    circle(mouseX, mouseY, 3);
+}
